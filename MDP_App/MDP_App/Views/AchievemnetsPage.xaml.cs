40d62879d8f319231a3f865269bef7053aeaf24e
iljaf <iljaf@LAPTOP-L76EK0UO>
2020-12-17 09:20:11 +0200

    win

--- MDP_App/MDP_App/Views/AchievemnetsPage.xaml.cs
+++ MDP_App/MDP_App/Views/AchievemnetsPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,20 @@ namespace MDP_App.Views
         public AchievemnetsPage()
         {
             InitializeComponent();
+            TapGestureRecognizer tap = new TapGestureRecognizer();
+            tap.Tapped += Tap_Tapped;
+            dzet.GestureRecognizers.Add(tap);
+
         }
+
+        private async void Tap_Tapped(object sender, EventArgs e)
+        {
+            var wiki = await DisplayAlert("Глянь игрока!", "Посмотри данные о игроке, продолжить?", "Да", "Нет");
+            if (wiki == true)
+            {
+                await Browser.OpenAsync("https://pvp.gg/player/f5e68cace4c2324356babb8024af5851/", BrowserLaunchMode.SystemPreferred);
+            }
+        }
+
     }
 }
--- MDP_App/MDP_App/Views/Cloud.xaml.cs
+++ MDP_App/MDP_App/Views/Cloud.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,20 @@ namespace MDP_App.Views
         public Cloud()
         {
             InitializeComponent();
+            TapGestureRecognizer tap = new TapGestureRecognizer();
+            tap.Tapped += Tap_Tapped;
+            cloud.GestureRecognizers.Add(tap);
+
         }
+
+        private async void Tap_Tapped(object sender, EventArgs e)
+        {
+            var wiki = await DisplayAlert("Глянь игрока!", "Посмотри данные о игроке, продолжить?", "Да", "Нет");
+            if (wiki == true)
+            {
+                await Browser.OpenAsync("https://wfts.su/profile/-Кл4уд.", BrowserLaunchMode.SystemPreferred);
+            }
+        }
+
     }
 }
